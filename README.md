d3297207dff29098fb05cef42f4935e363582f5d
Mgen <m@mgenware.com>
2026-07-29 17:39:56 +0800

    v2.0.0

--- README.md
+++ README.md
@@ -4,31 +4,31 @@
 
 - Generate a unique path in app temporary directory.
 - Generate a unique file name.
-- Clear all contents in app temporary directory (Android and iOS only).
+- Clear all contents in app temporary directory.
 
 ## Usage
 
 ```dart
-import 'package:app_tmp_path/src/app_tmp_path.dart';
+import 'package:app_tmp_path/app_tmp_path.dart';
 
 void main() async {
   // Generate a unique path in app temporary directory.
-  print(await tmpPath());
+  print(await createAppTmpPath());
   // /var/folders/q5/yvcxrtbn7mq5h4r4zvjhp_v40000gn/T/e8d9cbbcc1944c32a68d3d3739618dcb1679847188986
 
   // You can also specify a prefix for the generated path.
-  print(await tmpPath(prefix: 'test'));
+  print(await createAppTmpPath(prefix: 'test'));
   // /var/folders/q5/yvcxrtbn7mq5h4r4zvjhp_v40000gn/T/test399b42ececa84e069cf582b0adf1cea61679847188991
 
   // If you want to generate a unique file name, you can use tmpFileName() function.
   print(tmpFileName() + '.txt');
   // 30937c061c944d059dfc298242ef1e211679847188991.txt
 
-  // You can also clean the app's temporary directory (Android and iOS only).
+  // You can also clean the app's temporary directory.
   await cleanAppTmpDir();
 
-  // To get the path of the app's temporary directory, you can use getAppTmpDir() function.
+  // To get the path of the app's temporary directory, you can use initAppTmpDir() function.
   // This calls getTemporaryDirectory() from path_provider package internally.
-  print(await getAppTmpDir());
+  print(await initAppTmpDir());
 }
 ```
---
+++ lib/src/app_tmp_path.dart
@@ -0,0 +1,79 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:path/path.dart' as p;
+import 'package:path_provider/path_provider.dart';
+import 'package:uuid/uuid.dart';
+
+final _uuid = Uuid();
+String? _cachedSysTmpPath;
+String? _cachedAppTmpPath;
+const _appDirName = '_app';
+
+/// Returns `<tmp_dir>/_app`. If the directory does not exist, it will be created.
+Future<String> initAppTmpDir({bool? skipCreation}) async {
+  _cachedSysTmpPath ??= (await getTemporaryDirectory()).path;
+  _cachedAppTmpPath ??= p.join(_cachedSysTmpPath!, _appDirName);
+  if (skipCreation != true && !await Directory(_cachedAppTmpPath!).exists()) {
+    await Directory(_cachedAppTmpPath!).create(recursive: true);
+  }
+  return _cachedAppTmpPath!;
+}
+
+/// Returns a temporary file name.
+String tmpFileName() {
+  return '${_uuid.v4().replaceAll('-', '')}${DateTime.now().millisecondsSinceEpoch}';
+}
+
+/// Returns a temporary path in the `<tmp_dir>/_app`. You can use that to create a temporary file or directory.
+Future<String> createAppTmpPath({String prefix = ''}) async {
+  final tmpName = tmpFileName();
+  return p.join(await initAppTmpDir(), '$prefix$tmpName');
+}
+
+/// Cleans `<tmp_dir>/_app`. It deletes all files and directories in the temporary directory.
+Future<void> cleanAppTmpDir() async {
+  final appTmpDir = await initAppTmpDir(skipCreation: true);
+  final dir = Directory(appTmpDir);
+  if (await dir.exists()) {
+    await dir.delete(recursive: true);
+  }
+  // Recreate the directory after deletion.
+  await dir.create(recursive: true);
+}
+
+/// Cleans the app's root temporary directory. It deletes all files and directories in the temporary directory.
+Future<void> cleanAppRootTmpDir() async {
+  await _clearDirSilently(await getTemporaryDirectory());
+}
+
+/// Forces the `<tmp_dir>/_app` to be set to a specific path. This is useful for testing purposes.
+void setAppTmpDir(String path) {
+  _cachedAppTmpPath = path;
+}
+
+Future<void> _clearDirSilently(Directory dir) async {
+  try {
+    final entities = await dir.list().toList();
+    for (final entity in entities) {
+      final path = entity.path;
+      if (entity is File) {
+        try {
+          await entity.delete();
+        } catch (err) {
+          debugPrint('_cleanDirSilently: deleting file failed: $err, $path');
+        }
+      } else if (entity is Directory) {
+        try {
+          await entity.delete(recursive: true);
+        } catch (err) {
+          debugPrint(
+            '_cleanDirSilently: deleting directory failed: $err, $path',
+          );
+        }
+      }
+    }
+  } catch (err) {
+    debugPrint('_cleanDirSilently failed: $err');
+  }
+}
